39c727840ed33562a2d2e621525790d4afee9a07
Pham Duy Minh <minhpd@Phams-MacBook-Pro.local>
2023-12-13 16:23:29 +0700

    t

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+public class ContainerWithMostWater {
+  public int maxArea(int[] height) {
+    int left = 0;
+    int right = height.length - 1;
+    int maxArea = height[left] * height[right];
+    while(left< right) {
+      if(height[left] < height[right]) {
+        left++;
+        maxArea = Math.max(maxArea, height[left] * (right - left));
+      }
+      else {
+        right--;
+        maxArea = Math.max(maxArea, height[right] * (right - left));
+      }
+    }
+
+    return 
+  }
+}
