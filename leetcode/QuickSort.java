aa49db6322b0c53b905d557408035c7ab6577610
minh pham <hadtnt71@gmail.com>
2024-02-06 21:30:41 +0700

    update folder

--- leetcode/QuickSort.java
+++ leetcode/QuickSort.java
@@ -1,81 +1,44 @@
-import java.io.*;
-
-class QuickSort {
-
-  static {
-    System.out.println("This is quicksort");
+public class Quicksort {
+  void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp; 
   }
 
-	// A utility function to swap two elements
-	static void swap(int[] arr, int i, int j)
-	{
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+  int partition(int[] arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low-1 ;
+    for(int j = low; j < high ; j++ ) {
+      if(arr[j] < pivot) {
+        i++;
+        swap(arr,i,j);
+      }
+    }
 
-	// This function takes last element as pivot,
-	// places the pivot element at its correct position
-	// in sorted array, and places all smaller to left
-	// of pivot and all greater elements to right of pivot
-	static int partition(int[] arr, int low, int high)
-	{
-		// Choosing the pivot
-		int pivot = arr[high];
+    swap(arr,i+1,high);
 
-		// Index of smaller element and indicates
-		// the right position of pivot found so far
-		int i = (low - 1);
+    return (i+1);
+  }
 
-		for (int j = low; j <= high - 1; j++) {
+  void quickSort(int[] arr, int low, int high) {
+    if(low<high) {
+       // pi is partitioning index, arr[p]
+        // is now at right place
+        int pi = partition(arr, low, high);
 
-			// If current element is smaller than the pivot
-			if (arr[j] < pivot) {
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high); 
+    }
+  }
 
-				// Increment index of smaller element
-				i++;
-				swap(arr, i, j);
-			}
-		}
-		swap(arr, i + 1, high);
-		return (i + 1);
-	}
-
-	// The main function that implements QuickSort
-	// arr[] --> Array to be sorted,
-	// low --> Starting index,
-	// high --> Ending index
-	static void quickSort(int[] arr, int low, int high)
-	{
-		if (low < high) {
-
-			// pi is partitioning index, arr[p]
-			// is now at right place
-			int pi = partition(arr, low, high);
-
-			// Separately sort elements before
-			// partition and after partition
-			quickSort(arr, low, pi - 1);
-			quickSort(arr, pi + 1, high);
-		}
-	}
-	// To print sorted array
-	public static void printArr(int[] arr)
-	{
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-	}
-
-	// Driver Code
-	public static void main(String[] args)
-	{
-		int[] arr = { 10, 7, 8, 9, 1, 5 };
-		int N = arr.length;
-
-		// Function call
-		quickSort(arr, 0, N - 1);
-		System.out.println("Sorted array:");
-		printArr(arr);
-	}
+  public static void main(String[] args) {
+    Quicksort qs = new Quicksort();
+    int[] arr = {10, 7, 8, 9, 1, 5};
+    qs.quickSort(arr,0,arr.length-1);
+    for(int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
 }
