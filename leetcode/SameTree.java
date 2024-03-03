bcdc0cd0e954bb5ec882a0396f0fc3b0b9d37807
minh pham <hadtnt71@gmail.com>
2024-03-03 09:00:15 +0700

    3/3

---
+++ leetcode/SameTree.java
@@ -0,0 +1,24 @@
+public class SameTree {
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if(p == null && q == null) {
+      return true;
+    }
+    if( p== null || q==null || (p.val != q.val)) {
+      return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+  public static void main(String[] args) {
+    SameTree st = new SameTree();
+    TreeNode p = new TreeNode(1);
+    p.left = new TreeNode(2);
+    p.right = new TreeNode(3);
+    TreeNode q = new TreeNode(1);
+    q.left = new TreeNode(2);
+    // q.right = new TreeNode(null);
+    q.right = null;
+    System.out.println(st.isSameTree(p, q));
+  }
+}
