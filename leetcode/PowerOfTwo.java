cb19f7eb6a9b52d36e42cb0d02b187ec6c0c2019
minh pham <hadtnt71@gmail.com>
2024-03-26 08:35:52 +0700

    26/03

---
+++ leetcode/PowerOfTwo.java
@@ -0,0 +1,18 @@
+public class PowerOfTwo {
+  // public boolean isPowerOfTwo(int n) {
+  //     if(n==1) {
+  //         return true;
+  //     }
+
+  //     return n > 0 && n % 2 ==0 ? isPowerOfTwo(n/2) : false;
+  // }
+
+  public boolean isPowerOfTwo(int n) {
+    return n > 0 && Integer.bitCount(n) == 1;
+  }
+  
+  public static void main(String[] args) {
+      PowerOfTwo obj = new PowerOfTwo();
+      System.out.println(obj.isPowerOfTwo(16));
+  }
+}
